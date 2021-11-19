748726bca524f93e5e9b3c38d7a3549e8c48bb93
jgumerove <josh.gumerove@gmail.com>
2021-11-19 16:14:53 -0500

    create container for map

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import CountryContainer from './CountryContainer'
+import WorldMapContainer from './WorldMapContainer'
 
 class App extends Component {
     render() {
         return (
             <div>
                 Hello world
+                <WorldMapContainer />
                 <CountryContainer />
             </div>
         )
---
+++ src/components/WorldMapContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class WorldMapContainer extends Component {
+    render() {
+        return (
+            <div>
+                Map Goes here
+            </div>
+        )
+    }
+}
+
+export default WorldMapContainer
