990c2d809663a37c799430dffeebe357743ed8d1
jgumerove <josh.gumerove@gmail.com>
2021-11-19 16:13:53 -0500

    create countries container

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import CountryContainer from './CountryContainer'
 
 class App extends Component {
     render() {
         return (
             <div>
                 Hello world
+                <CountryContainer />
             </div>
         )
     }
---
+++ src/components/CountryContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class CountryContainer extends Component {
+    render() {
+        return (
+            <div>
+                Countries go here
+            </div>
+        )
+    }
+}
+
+export default CountryContainer
