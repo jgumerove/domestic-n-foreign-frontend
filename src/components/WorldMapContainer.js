214d2f6e34db44110f71ce2ed22633ddebb8c3ba
jgumerove <josh.gumerove@gmail.com>
2021-11-20 10:37:58 -0500

    add source credit

--- src/components/WorldMapContainer.js
+++ src/components/WorldMapContainer.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import L from 'leaflet';
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import L from 'leaflet';
+
+//source: https://github.com/PaulLeCam/react-leaflet/issues/453
 let DefaultIcon = L.icon({
   iconUrl: icon,
   shadowUrl: iconShadow
