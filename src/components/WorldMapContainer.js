51b54d3673b95384c45794883390beddb41561eb
jgumerove <josh.gumerove@gmail.com>
2021-11-20 11:53:06 -0500

    add no wrap feature to map

--- src/components/WorldMapContainer.js
+++ src/components/WorldMapContainer.js
@@ -14,9 +14,12 @@ let DefaultIcon = L.icon({
 
 L.Marker.prototype.options.icon = DefaultIcon;
 
+
 class WorldMapContainer extends Component {
     render() {
+
         const position = [51.505, -0.09]
+
         const test = [{
             lat: 51.505,
             long: -0.09,
@@ -26,24 +29,21 @@ class WorldMapContainer extends Component {
             long: 37.6173,
             location: "Moscow"
         }]
-        return (
-      <MapContainer center={position} zoom={3} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+
+    return (
+      <MapContainer center={position} zoom={2} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          noWrap={true} //to fix continent reptition
       />
-      {test.map((p) =>{
-         return (<Marker position={[p.lat, p.long]} >
-              <Popup>
-                  <span>{p.location}</span>
-              </Popup>
-          </Marker>)
-      })}
-      {/* <Marker position={position}>
-        <Popup>
-           <span>Look at the Popup</span>
-        </Popup>
-      </Marker> */}
+        {test.map((p) =>{
+           return (<Marker position={[p.lat, p.long]} >
+                      <Popup>
+                       <span>{p.location}</span>
+                     </Popup>
+                 </Marker>)
+                  })}
     </MapContainer>
         )
     }
