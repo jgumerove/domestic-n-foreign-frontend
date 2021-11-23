7571681b3ae6068e03e9aba2a7095855b4808bf2
jgumerove <josh.gumerove@gmail.com>
2021-11-23 17:21:08 -0500

    add base count

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -3,13 +3,19 @@ import { Link } from 'react-router-dom'
 
 class CountryCard extends Component {
     render() {
-        const {name, flag, id} = this.props
+        const {name, flag, id, baseCount} = this.props
         return (
             <div className="country-card">
                 <div>
+                 <h5>Bases: {baseCount}</h5>
+                </div>
+                <div>
                 <Link to={`/countries/${id}`}><img src={flag} alt={name} /></Link>
                 </div>
+                <div>
                  <Link to={`/countries/${id}`}><span>{name}</span></Link>
+                </div>
+                 
             </div>
         )
     }
