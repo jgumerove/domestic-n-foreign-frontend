8c7b1ea75195458a015ae78293551a740ff86754
jgumerove <josh.gumerove@gmail.com>
2021-11-23 18:34:02 -0500

    edit fetch country

--- src/actions/fetchCountry.js
+++ src/actions/fetchCountry.js
@@ -1,6 +1,6 @@
 export const fetchCountry = (id) => {
     return dispatch => fetch(`http://localhost:3000/countries/${id}`)
     .then(res => res.json())
-    .then(countries => dispatch({type: "FETCH_COUNTRY", payload: country})
+    .then(country => dispatch({type: "FETCH_COUNTRY", payload: country})
     )
 }
