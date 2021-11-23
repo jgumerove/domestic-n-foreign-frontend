35ea763d7c921a784a6232d0522fc84361597c35
jgumerove <josh.gumerove@gmail.com>
2021-11-22 19:07:35 -0500

    add link to all countries

--- src/containers/CountryShow.js
+++ src/containers/CountryShow.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
 
 export class CountryShow extends Component {
     render() {
         return (
             <div>
-                Country Bases here
+                <span>Country Bases here</span>
+                <br></br>
+                <Link to="/countries">Go back</Link>
             </div>
         )
     }
