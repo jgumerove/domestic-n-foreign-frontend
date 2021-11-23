d1a413fe35f94f3036c776c69bd56cfbd41136c0
jgumerove <josh.gumerove@gmail.com>
2021-11-23 18:51:30 -0500

    update reducer

--- src/reducers/countriesReducer.js
+++ src/reducers/countriesReducer.js
@@ -1,11 +1,17 @@
 
 
 export const countriesReducer = (state = {
-    countries: []
+    countries: [],
+    selectedCountry: {
+        name: "",
+        places: []
+    }
 }, action) => {
     switch(action.type){
         case "FETCH_COUNTRIES":
             return {...state, countries: action.payload};
+        case "FETCH_COUNTRY":
+            return {...state, selectedCountry: action.payload}
         default:
             return state
     }
