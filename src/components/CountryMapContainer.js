79629b544cafe5c3cc364023bf2ccaea891c30c4
jgumerove <josh.gumerove@gmail.com>
2021-11-23 20:35:34 -0500

    add leaflet to country map

--- src/components/CountryMapContainer.js
+++ src/components/CountryMapContainer.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { fetchCountry } from '../actions/fetchCountry';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import nutcracker from '../Static/nutcracker.png'
+import L from 'leaflet';
+
+let DefaultIcon = L.icon({
+    iconUrl: nutcracker,
+    iconSize: [20, 45], //to make icon size smaller
+    iconAnchor: [10, 41] //to fix position on zoom-out
+  });
+
+  L.Marker.prototype.options.icon = DefaultIcon;
 
 
 class CountryMapContainer extends Component {
@@ -15,7 +26,18 @@ class CountryMapContainer extends Component {
          const { selectedCountry } = this.props
         return (
             <div>
-                Map here {selectedCountry.name}
+               <MapContainer center={[51.505, -0.09]} zoom={2} scrollWheelZoom={false}>
+                    <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    noWrap={true}
+                                    />
+                    <Marker position={[51.505, -0.09]}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                    </Marker>
+                </MapContainer>
                 <br></br>    
                 <Link to="/countries">Go back</Link>
             </div>
