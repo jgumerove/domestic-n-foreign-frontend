8f8a5b3bd6351342b29221e2a64fe77453f0cc1f
jgumerove <josh.gumerove@gmail.com>
2021-11-24 13:13:34 -0500

    add link to about

--- src/components/CountriesIndex.js
+++ src/components/CountriesIndex.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import CountryContainer from '../containers/CountryContainer'
 import Greeting from './Greeting'
 import WorldMapContainer from '../containers/WorldMapContainer'
+import { Link } from 'react-router-dom'
+
 
 class CountriesIndex extends Component {
     render() {
@@ -10,6 +12,7 @@ class CountriesIndex extends Component {
                 <Greeting />
                 <WorldMapContainer /><br/>
                 <CountryContainer />
+                <div><Link to="/about"><h3>About Us</h3></Link></div>  
             </div>
         )
     }
