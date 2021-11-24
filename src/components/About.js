f8e20c0d539e2b1527b781c23343e8d39fc1aa96
jgumerove <josh.gumerove@gmail.com>
2021-11-24 17:28:07 -0500

    add functinal component

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import BackLink from './BackLink'
+import Description from './Description'
 
 
 function About() {
     return (
         <div>
-            <h2>About Us</h2><br/>
+            <h2>About Us</h2>
+            <Description />
             <BackLink />
         </div>
     )
---
+++ src/components/Description.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Description() {
+    return (
+        <h4>
+             Military Mistletoe is an interactive map application that tracks all military<br/>
+             bases around the world using a Christmas theme.  Simply click on a country via the<br/>
+             nutcracker icon on the map or the countries index card to view all the foreign military<br/>
+             bases the country has throughout the world. Then view those military bases via an interactive map.
+            
+        </h4>
+    )
+}
+
+export default Description
